ddedae0cfec73d94d17e25e0392f2d66569010f0
sarangsyam <167174059+sarangsyam@users.noreply.github.com>
2024-07-05 07:16:35 +0000

    asdasda

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import PaymentNotice from './PaymentNotice.js'
+import PaymentNotice from './PaymentNotice.jsx'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
